98143d0a0cfd4dd0ee74c8566b20fda48b9cf7d4
Numan <numan@foolography.com>
2022-04-03 15:59:34 +0200

    equal fun edited in City.kt

--- app/src/main/java/com/simplertutorials/android/wheathograophy/domain/City.kt
+++ app/src/main/java/com/simplertutorials/android/wheathograophy/domain/City.kt
@@ -4,4 +4,9 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class City(val name: String, val weather: Weather? = null) : Parcelable
+data class City(val name: String, val weather: Weather? = null) : Parcelable {
+
+    override fun equals(other: Any?): Boolean {
+        return other is City && name == other.name
+    }
+}
