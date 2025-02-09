d89f3c406915ebf3bbfc0044f0ec64f3bf83d618
Numan <numanfidan@gmail.com>
2025-02-10 00:54:45 +0100

    removed rx and retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,20 +89,6 @@ dependencies {
     implementation(libs.koin)
 
 
-    //retrofit and Gson
-    implementation ("com.google.code.gson:gson:2.11.0")
-    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
-    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.3.0")
-
-    //OkHttp and OkHttpLoggingInterceptor
-    implementation ("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
-
-    //Rx
-    implementation ("io.reactivex.rxjava2:rxandroid:2.1.0")
-    implementation ("io.reactivex.rxjava2:rxjava:2.2.0")
-
     implementation ("com.google.android.material:material:1.12.0")
 
     //Swipe To Refresh
